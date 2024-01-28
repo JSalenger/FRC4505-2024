c14695b920b063e736fe902c1fc37783dfafc07c
Jon Salenger <jbsalenger@mcdonogh.org>
2024-01-28 13:35:39 -0500

    working teleop drive

---
+++ src/main/java/frc/robot/commands/SwerveCmdJoystick.java
@@ -0,0 +1,80 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DrivebaseConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SwerveCmdJoystick extends Command {
+    private final SwerveSubsystem swerveSubsystem;
+    private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
+    private final Supplier<Boolean> fieldOrientedFunction;
+    private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+
+    public SwerveCmdJoystick(SwerveSubsystem swerveSubsystem, Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> turningSpeedFunction, Supplier<Boolean> fieldOrientedFunction) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.xSpdFunction = xSpdFunction;
+        this.ySpdFunction = ySpdFunction;
+        this.turningSpdFunction = turningSpeedFunction;
+        this.fieldOrientedFunction = fieldOrientedFunction;
+        this.xLimiter = new SlewRateLimiter(DrivebaseConstants.kTeleMaxDriveAccelerationMetersPerSecond);
+        this.yLimiter = new SlewRateLimiter(DrivebaseConstants.kTeleMaxDriveAccelerationMetersPerSecond);
+        this.turningLimiter = new SlewRateLimiter(DrivebaseConstants.kTeleMaxAngularAccelerationMetersPerSecond);
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        //1. Get real-time joystick vals
+        double xSpeed = xSpdFunction.get();
+        double ySpeed = ySpdFunction.get();
+        double turningSpeed = turningSpdFunction.get();
+
+        //2. apply deadband
+        xSpeed = Math.abs(xSpeed) > OIConstants.kDeadband ? xSpeed : 0.0;
+        ySpeed = Math.abs(ySpeed) > OIConstants.kDeadband ? ySpeed : 0.0;
+        turningSpeed = Math.abs(turningSpeed) > OIConstants.kDeadband ? turningSpeed : 0.0;
+
+        //3. make driving smoother
+        xSpeed = xLimiter.calculate(xSpeed) * DrivebaseConstants.kTeleMaxDriveSpeed;
+        ySpeed = yLimiter.calculate(ySpeed) * DrivebaseConstants.kTeleMaxDriveSpeed;
+        turningSpeed = turningLimiter.calculate(turningSpeed) * DrivebaseConstants.kTeleMaxAngularSpeed;
+
+        //4. construct desired chassis speeds
+        ChassisSpeeds chassisSpeeds;
+        if (fieldOrientedFunction.get()) {
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
+        } else {
+            chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+        }
+
+        //5. convert to swerve module states
+        SwerveModuleState[] moduleStates = DrivebaseConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+        //6. apply
+        swerveSubsystem.setModules(moduleStates);
+
+        // for now, try to set all the wheels to repeatable locations
+        // SwerveModuleState testState = new SwerveModuleState(.5*0.01, new Rotation2d(0));
+        // SwerveModuleState[] testModuleStates = {testState, testState, testState, testState};
+        // swerveSubsystem.setModules(testModuleStates);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveSubsystem.stopModules();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,118 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.kauailabs.navx.frc.AHRS.SerialDataType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivebaseConstants;
+
+public class SwerveSubsystem extends SubsystemBase {
+    double fl = -0.16873788666742;
+    // double[] offsets = {1.347+2.988, 0.688, 1.977+0.16, 0.759+0.08};
+    // double[] offsets = {
+    //     4.979-0.224-Math.PI/2, 
+    //     3.848+Math.PI/2, 
+    //     2.661-0.07-Math.PI/2, 
+    //     1.242-0.19+Math.PI/2};
+    double[] offsets = {0.834-Math.PI/2, 0.657+Math.PI/2, 4.139-Math.PI/2, 0.500+Math.PI/2};
+    // switching 1 and 3 seems to make turning directions correct
+    private final SwerveModule frontLeft = new SwerveModule(3, false, true, offsets[2], false);
+    private final SwerveModule frontRight = new SwerveModule(2, true, true, offsets[1], false);
+    private final SwerveModule backRight = new SwerveModule(1, false, true, offsets[0], false);
+    private final SwerveModule backLeft = new SwerveModule(4, true, true, offsets[3], false);
+
+    private final AHRS gyro = new AHRS(Port.kUSB1, SerialDataType.kProcessedData, Byte.parseByte("100"));
+
+    public SwerveSubsystem() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                zeroHeading(); // requires a delay so that the gyroscope can start up & calibrate
+            } catch (Exception e) {}    
+        }).start();
+    }
+
+    public void zeroHeading() {
+        gyro.reset();
+    }
+
+    public double getHeading() {
+        // -angle because google told me that it should be counterclockwise positive
+        return Math.IEEEremainder(-gyro.getAngle(), 360);
+    }
+
+    public Rotation2d getRotation2d() {
+        return Rotation2d.fromDegrees(getHeading());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Robot Heading", getHeading());  // -frontLeft.getAbsoluteEncoderRad()
+        SmartDashboard.putNumber("Swerve["+frontLeft.moduleID+"] Absolute Encoder Val (RAD)", frontLeft.getAbsoluteEncoderRad());
+        SmartDashboard.putNumber("Swerve["+frontRight.moduleID+"] Absolute Encoder Val (RAD)", frontRight.getAbsoluteEncoderRad());
+        SmartDashboard.putNumber("Swerve["+backRight.moduleID+"] Absolute Encoder Val (RAD)", backRight.getAbsoluteEncoderRad());
+        SmartDashboard.putNumber("Swerve["+backLeft.moduleID+"] Absolute Encoder Val (RAD)", backLeft.getAbsoluteEncoderRad());
+
+        SmartDashboard.putNumber("Swerve["+frontLeft.moduleID+"] Abs Enc (DEG)", Math.toDegrees(frontLeft.getAbsoluteEncoderRad()));
+        SmartDashboard.putNumber("Swerve["+frontRight.moduleID+"] Abs Enc (DEG)", Math.toDegrees(frontRight.getAbsoluteEncoderRad()));
+        SmartDashboard.putNumber("Swerve["+backRight.moduleID+"] Abs Enc (DEG)", Math.toDegrees(backRight.getAbsoluteEncoderRad()));
+        SmartDashboard.putNumber("Swerve["+backLeft.moduleID+"] Abs Enc (DEG)", Math.toDegrees(backLeft.getAbsoluteEncoderRad()));
+
+        SmartDashboard.putNumber("Swerve["+frontLeft.moduleID+"] Abs Enc Raw (RAD)", frontLeft.getAbsoluteEncoderRadRaw());
+        SmartDashboard.putNumber("Swerve["+frontRight.moduleID+"] Abs Enc Raw (RAD)", frontRight.getAbsoluteEncoderRadRaw());
+        SmartDashboard.putNumber("Swerve["+backRight.moduleID+"] Abs Enc Raw", backRight.getAbsoluteEncoderRadRaw());
+        SmartDashboard.putNumber("Swerve["+backLeft.moduleID+"] Abs Enc Raw (RAD)", backLeft.getAbsoluteEncoderRadRaw());
+    } 
+
+    public void stopModules() {
+        frontLeft.stop();
+        frontRight.stop();
+        backLeft.stop();
+        backRight.stop();
+    }
+
+    public void setModules(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DrivebaseConstants.kMaxSpeedMetersPerSecond); // TODO: MEASURE MAX SPEED
+        frontLeft.setDesiredState(desiredStates[0]);
+        frontRight.setDesiredState(desiredStates[1]);
+        backRight.setDesiredState(desiredStates[2]);
+        backLeft.setDesiredState(desiredStates[3]);
+    }
+
+    // FOR MODULE TESTING
+    public void testModule(int id, double speed, boolean usingAngleMotor) {
+        SwerveModule mod;
+        switch(id) {
+            case 1: mod = frontLeft; break;
+            case 2: mod = frontRight; break;
+            case 3: mod = backRight; break;
+            case 4: mod = backLeft; break;
+            default: mod = frontLeft; break;
+        }
+        if(usingAngleMotor) {
+            mod.setTurningMotor(speed);
+        } 
+        else {
+            mod.setDriveMotor(speed);
+        }
+    }
+
+    public void setOffsets() {
+        frontLeft.setOffset(frontLeft.getAbsoluteEncoderRadRaw()+Math.PI/2);
+        frontRight.setOffset(frontRight.getAbsoluteEncoderRadRaw()-Math.PI/2);
+        backLeft.setOffset(backLeft.getAbsoluteEncoderRadRaw()+Math.PI/2);
+        backRight.setOffset(backRight.getAbsoluteEncoderRadRaw()-Math.PI/2);
+    }
+}
