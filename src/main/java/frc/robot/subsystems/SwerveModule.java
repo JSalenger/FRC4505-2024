060c4085b2c8a397582b12e53fd74ea314c047d4
Jon Salenger <jbsalenger@mcdonogh.org>
2024-10-17 15:56:49 -0400

    fixed drivetrain current limits

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -49,7 +49,7 @@ public class SwerveModule extends SubsystemBase {
         this.turningMotor.setIdleMode(IdleMode.kBrake);
         this.driveMotor.setIdleMode(IdleMode.kBrake);  // coast
 
-        this.turningMotor.setSmartCurrentLimit(40);
+        this.turningMotor.setSmartCurrentLimit(30);
         this.driveMotor.setSmartCurrentLimit(40);
         
         this.driveEncoder = this.driveMotor.getEncoder();
